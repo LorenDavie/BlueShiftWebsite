2a5b095a98f5b31b10e38d5e2601dab3d2993a7c
Loren Davie <code@axilent.com>
2020-01-25 18:26:36 -0500

    config problem
    Signed-off-by: Loren Davie <code@axilent.com>

--- blueweb/settings.py
+++ blueweb/settings.py
@@ -86,7 +86,7 @@ if os.environ.get('BLUESHIFT_RUNLOCAL', None):
         }
     }
 else:
-    DATABASES = dj_database_url.config()
+    DATABASES['default'] = dj_database_url.config()
 
 
 # Password validation
